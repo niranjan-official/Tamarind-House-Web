3d6ced652fe656a6e62b832402ac70bd77e742af
niranjan-official <niranjansabarinath1521@gmail.com>
2026-01-20 22:31:48 +0530

    loading change

--- app/(dashboard)/loading.jsx
+++ app/(dashboard)/loading.jsx
@@ -4,7 +4,6 @@ export default function Loading() {
   return (
     <div className="w-full h-[80vh] flex flex-col justify-center items-center gap-4">
       <div class="loader"></div>
-      <p className="text-th-medium-green font-medium">Loading...</p>
     </div>
   )
 }
